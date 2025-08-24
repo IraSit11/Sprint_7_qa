192119119eed9c9b3a04146b08582a588510ac48
IraSit11 <you@example.com>
2025-08-24 17:58:29 +0300

    дополнение к правкам после первого ревью

---
+++ src/main/java/Url.java
@@ -0,0 +1,8 @@
+public class Url {
+    public static final String CREATE_COURIER = "/api/v1/courier";
+    public static final String LOGIN_COURIER = "/api/v1/courier/login";
+    public static final String DELETE_COURIER = "/api/v1/courier/";
+    public static final String CREATE_ORDER = "/api/v1/orders";
+    public static final String DELETE_ORDER = "/api/v1/orders/cancel";
+    public static final String GET_LIST_ORDERS = "/api/v1/orders";
+}
